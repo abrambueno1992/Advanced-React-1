b4ed3f32d1d54e883a3ec1d6d2043bc9331e27df
Abraham Bueno <abueno@localhost.localdomain>
2019-12-16 21:17:48 -0500

    Added changes

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+// const hey
+import Link from "next/link"
+const Home = (props) => {
+    return (
+        <div>
+            <p>Hey</p>
+
+            
+        </div>
+    )
+}
+export default Home
